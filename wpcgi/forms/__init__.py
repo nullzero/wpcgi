818905006f9aef70948b5a1ba489293cb7fde1c6
nullzero <nullzero.free@gmail.com>
2014-03-27 03:38:39 -0400

    add language

--- wpcgi/forms/__init__.py
+++ wpcgi/forms/__init__.py
@@ -3,11 +3,16 @@
 from wtforms import SelectField
 from messages import msg
 
-
 def lang_default(*args, **kwargs):
     return SelectField(*args, choices=[
         ('th', msg['form-lang-th']),
-        ('en', msg['form-lang-en'])
+        ('en', msg['form-lang-en']),
+        ('de', msg['form-lang-de']),
+        ('fr', msg['form-lang-fr']),
+        ('it', msg['form-lang-it']),
+        ('ru', msg['form-lang-ru']),
+        ('vi', msg['form-lang-vi']),
+        ('zh', msg['form-lang-zh']),
     ], **kwargs)
 
 def fam_default(*args, **kwargs):
