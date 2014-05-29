3ba41bd38830baa07275001ddf6e896174decbb3
nullzero <nullzero.free@gmail.com>
2014-05-29 12:16:43 -0400

    localize contribtracker

--- wpcgi/i18n/contribtracker.py
+++ wpcgi/i18n/contribtracker.py
@@ -2,3 +2,19 @@
 # -*- coding: utf-8 -*-
 
 messages = {}
+
+messages['th'] = {
+    'contribtracker-title': u'เครื่องมือนับจำนวนคำ',
+    'contribtracker-tab-page': u'ใช้ชื่อหน้า',
+    'contribtracker-tab-content': u'ใช้เนื้อหา',
+    'contribtracker-title-label': u'ชื่อหน้า',
+    'contribtracker-title-placeholder': u'เช่น A',
+    'contribtracker-content-label': u'เนื้อหา',
+    'contribtracker-content-placeholder': u'เช่น [[สงครามโลกครั้งที่สอง]]เป็นสงครามระหว่างไทยกับอังกฤษ{{อ้างอิง}}',
+    'contribtracker-word-count': u'จำนวนคำ',
+    'contribtracker-button-submit': u'นับจำนวนคำ',
+}
+
+messages['en'] = {
+
+}
