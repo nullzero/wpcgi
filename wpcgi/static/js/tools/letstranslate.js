ee8352076e92470924b7e04d10153330f8bf97e3
Sorawee Porncharoenwase <nullzero.free@gmail.com>
2014-07-04 03:45:15 +0700

    letstranslate: fix autocomplete

--- wpcgi/static/js/tools/letstranslate.js
+++ wpcgi/static/js/tools/letstranslate.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#txt-lang").keyup(function(){
-        autocompleteTitle($("#txt-lang").val(), "#txt-ftitle", 0, "#txt-fam");
+        autocompleteTitle($("#txt-lang").val(), "#txt-ftitle", 0, $("#txt-fam").val());
     });
     $("#txt-lang").keyup();
     $("#txt-fam").keyup();
